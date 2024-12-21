8952cb03e4fed7424cfd35893b7d75d77507f51a
faiaz <faiazrex8@gmail.com>
2024-12-22 00:45:11 +0530

    22/12/2024

--- src/main/java/ProductDataTransferObject/AddProductDTO.java
+++ src/main/java/ProductDataTransferObject/AddProductDTO.java
@@ -11,50 +11,60 @@ public class AddProductDTO {
     private String title;
 
     public String getTitle() {
+
         return title;
     }
 
     public void setTitle(String title) {
+
         this.title = title;
     }
 
     private String description;
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
     }
 
     private String category;
 
     public String getCategory() {
+
         return category;
     }
 
     public void setCategory(String category) {
+
         this.category = category;
     }
 
     private String image;
 
     public String getImage() {
+
         return image;
     }
 
     public void setImage(String image) {
+
         this.image = image;
     }
 
     private double price;
 
     public double getPrice() {
+
         return price;
     }
 
     public void setPrice(double price) {
+
         this.price = price;
     }
 }
--- src/main/java/ProductModels/Product.java
+++ src/main/java/ProductModels/Product.java
@@ -18,40 +18,48 @@ public class Product extends ProductBaseModel{
     private String name;
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     private String description;
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
     }
 
     private double price;
 
     public double getPrice() {
+
         return price;
     }
 
     public void setPrice(double price) {
+
         this.price = price;
     }
 
     private String imageURL;
 
     public String getImageURL() {
+
         return imageURL;
     }
 
     public void setImageURL(String imageURL) {
+
         this.imageURL = imageURL;
     }
 
@@ -59,10 +67,12 @@ public class Product extends ProductBaseModel{
     private ProductCategory productCategory;
 
     public ProductCategory getProductCategory() {
+
         return productCategory;
     }
 
     public void setProductCategory(ProductCategory productCategory) {
+
         this.productCategory = productCategory;
     }
 }
